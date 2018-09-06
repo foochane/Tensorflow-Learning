73f242f6b65a282f4364d5b56213cacfa1de0fbc
foochane <fuchengkm@qq.com>
2018-09-06 21:49:52 +0800

    计算图图片

--- 03 第三讲 Tensorflow框架/tf3_1.py
+++ 03 第三讲 Tensorflow框架/tf3_1.py
@@ -3,3 +3,14 @@ a=tf.constant([1.0,2.0])
 b=tf.constant([3.0,4.0])
 result=a+b
 print result
+
+"""
+输出的结果为：
+Tensor("add:0", shape=(2,), dtype=float32)
+
+add ：节点名
+shape :维度信息，括号里只有一个数“2“，表示维度是1且一个维度里有两个元素
+dtpye :数据类型
+"""
+
+
